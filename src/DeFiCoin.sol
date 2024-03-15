150a180c3e2a9e0f693bb9d92e5777457d060a30
stefano <stefano1990@gmail.com>
2024-03-15 12:09:43 +0000

    Tag version of openzeppelin

--- src/DeFiCoin.sol
+++ src/DeFiCoin.sol
@@ -1,10 +1,21 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract DeFiCoin is ERC20 {
-    constructor() ERC20("DeFiCoin", "DFC") {
+contract DeFiCoin is ERC20, Ownable {
+    constructor() Ownable(msg.sender) ERC20("DeFiCoin", "DFC") {
         _mint(msg.sender, 1000 * 10 ** uint(decimals()));
     }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burn(address from, uint256 amount) public onlyOwner {
+        _burn(from, amount);
+    }
+
+    // function transfer(address recipient, uint256 amount) public virtual override returns (bool);
 }
